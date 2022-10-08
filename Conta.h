7601ea4a4320f7ae88a2ed92274fe9c6a3d8ef76
Francisco Pedro <109813896+franciscopeedro@users.noreply.github.com>
2022-10-08 13:44:39 -0300

    finish

--- Conta.h
+++ Conta.h
@@ -4,12 +4,12 @@ using namespace std;
 class Conta{
     private:
     float saldo;
-
+    
     public:
     Conta(float saldo);                   //Construtor
     void deposita(float saldo);           //Adiciona valor
     void saca(float valor);               //Subtrai valor do saldo
     float getSaldo();                     //Retorna o saldo
-    void transfere(Conta a,int valor);    //Transere para a outra conta um valor
+    void transfere(Conta a,int valor);    //Transere para outra conta um valor.
 
 };
